55cd2eeb6e2011cc6436f49026448ac5dfbd30e7
jmvillalobos <jvillalobos886@gmail.com>
2018-10-05 11:20:45 -0500

    Add files via upload

---
+++ mo.sh
@@ -0,0 +1,10 @@
+ca= cut -f 1,2,3 /Users/josemanuelvillalobosescobedo/WT_Injury.txt
+
+
+echo $ca
+
+to_print= cut -f 1 /Users/josemanuelvillalobosescobedo/WT_Injury.txt | sum
+
+echo "costo"  
+
+ $to_print
